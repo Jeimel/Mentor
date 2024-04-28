c2c2f1fa78e2e3b5d38e330b67cf30792c56ceb1
Felix Jablinski <jablinskifelix@gmail.com>
2024-04-28 14:07:29 +0200

    Add TicTacToe

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use mentor::{mcts::Search, TicTacToe};
+
 fn main() {
-    println!("Hello, world!");
+    let game = TicTacToe {
+        turn: 0,
+        grid: [0b_000_000_000, 0b_000_000_000],
+    };
+
+    let mut search = Search::new(game);
+    println!("{}", search.run());
 }
